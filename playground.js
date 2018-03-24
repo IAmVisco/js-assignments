894c5d690a4551e438d2f94cbb659acec3aec1e5
Alexander <sashok935@gmail.com>
2018-03-24 17:01:30 +0300

    Finish array tasks

--- playground.js
+++ playground.js
@@ -1,7 +1,5 @@
-function timeSpanToString(startDate, endDate) {
-    var timespan = new Date(endDate - startDate);    
-    return timespan.toISOString().slice(11, -1); 
+function propagateItemsByPositionIndex(arr) {
+   return arr.map(x => x.repeat(arr.indexOf(x) + 1));
 }
-startDate = new Date(2000,1,1,10,0,0);
-endDate = new Date(2000,1,1,11,0,0);
-console.log(timeSpanToString(startDate, endDate));
+var arr = [ 1];
+console.log(propagateItemsByPositionIndex(arr));
